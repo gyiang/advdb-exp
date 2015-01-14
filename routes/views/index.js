6c52644bf415b5d13d4d2374ab40a53f1c643a13
Gan Yi.ang <getbox@126.com>
2015-01-14 20:42:24 +0800

    增加编辑自己的报告， 增加查看自己发布的报告，

--- routes/views/index.js
+++ routes/views/index.js
@@ -4,10 +4,33 @@ exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res),
 		locals = res.locals;
-	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
+	// Init locals
 	locals.section = 'home';
+	locals.data = {
+		posts: []
+	};
+
+	if(req.user){
+		// Load the posts
+		view.on('init', function(next) {
+			
+			var q = keystone.list('Post').paginate({
+					page: req.query.page || 1,
+					perPage: 10,
+					maxPages: 10
+				})
+				.where('author',req.user._id)
+				.sort('-publishedDate')
+				.populate('author');
+
+					
+			q.exec(function(err, results) {
+				locals.data.posts = results;
+				next(err);
+			});
+			
+		});
+	}
 	
 	// Render the view
 	view.render('index');
