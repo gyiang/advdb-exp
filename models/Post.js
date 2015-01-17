3a3c04877fa460677265ceda4e1ee1b7499507d7
Gan Yi.ang <getbox@126.com>
2015-01-17 21:43:24 +0800

    设置author约束必有，publisher自带默认值

--- models/Post.js
+++ models/Post.js
@@ -14,8 +14,8 @@ var Post = new keystone.List('Post', {
 Post.add({
 	title: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	author: { type: Types.Relationship, ref: 'User', index: true },
-	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+	author: { type: Types.Relationship, ref: 'User', index: true,required: true ,initial:true},
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } ,default: Date.now },
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 50 },
 		extended: { type: Types.Html, wysiwyg: true, height: 200 }
